97136cb56c1f8a63ac0035b24999db0bfc0c2a20
Wensomt <50673033+Wensomt@users.noreply.github.com>
2024-08-18 16:23:37 +0200

    First Version of Patch
    Mechanics:
    - Removed Focus Punishment
    - No Boss Countering
    - When we summon Boss only our boss lvl goes up
    Tewi:
    - 2x faster bullets of charge attack (that mean 2x bigger range)
    - Every Boss attack got buff
    - Spells appear higher on screen and can x position of spawn is not constant also bullets are faster.
    Cirno:
    - Every Boss attack got some buffs
    Merlin:
    - Much faster Charge Speed
    Marisa:
    - Slower Charge Speed
    Mystia:
    - Green Bullets from Boss attacks are faster and 2x more of them

---
+++ NoNerd/patch.js
@@ -0,0 +1,11 @@
+{
+    "id": "Th9NoNerd",
+    "servers": [
+        "https://mirrors.thpatch.net/Wensomt/Th9NoNerd/"
+    ],
+    "title": "Remove From Touhou PoFV Nerd mechanics like punishment for using focus, boss countering, and few buffs/nerfs to characters",
+    "dependencies": [
+        "nmlgc/base_tsa",
+        "zero318/ZeroEclExtensions"
+    ]
+}
---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+    "contact": "@Wensomt",
+    "id": "Wensomt",
+    "patches": {
+        "Th9NoNerd": "Remove From Touhou PoFV Nerd mechanics like punishment for using focus, boss countering, and few buffs/nerfs to characters",
+    },
+    "servers": [
+        "https://mirrors.thpatch.net/Wensomt/"
+    ],
+    "title": "Wensomt's Repository"
+}
