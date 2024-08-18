ea8e2fbb1c1d096dcdd72dbd88ba8d4fefdf6bfa
Wensomt <50673033+Wensomt@users.noreply.github.com>
2024-08-18 16:14:29 +0200

    Initial commit

---
+++ th09.v1.50a.js
@@ -0,0 +1,20 @@
+{
+  "binhacks": {
+    "Boss_No_Uno": {
+      "addr": 0x403ea5, 
+      "code": "8B4E04",
+    },
+    "Free_Focus": {
+      "addr": 0x410861, 
+      "code": "E8AA0CFFFF",
+    },
+    "Our_Boss_1": {
+      "addr": 0x403f94, 
+      "code": "B901000000",
+    },
+    "Our_Boss_2": {
+      "addr": 0x403f99, 
+      "code": "83FB0190",
+    },
+  },
+}
