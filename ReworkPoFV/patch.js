1abed5a942d07c6643fe5013c6002743a6f668d2
Wensomt <50673033+Wensomt@users.noreply.github.com>
2024-09-01 22:01:32 +0200

    1.0.0
    # Gameplay Changes:
    - While focusing there is no cap for number of fairies
    - ' Z ' can be hold down to keep shooting, while ' X ' is responsible for charging, cannot use bomb -now
    - Now both players bosses can co-exist without countering each other
    - Enemy boss summon no longer increase our Boss LVL
    - 2 Lvl Spell clear 2x smaller area around us
    - 3 Lvl Spell clear as much as old 2 Lvl Spell
    # Characters Changes:
    ## Reimu:
    ### Spells:
    - Shoot a lot of less Bullets
    - No Longer Scale to point that it shoot 3 patterns (max 2)
    - 2 Lvl Spell is slower
    ### Boss:
    - Attack where she send few layers of amulets now no longer scale with difficulty but with Boss Lvl
    ## Marisa:
    - Slower Charge Speed
    ## Youmu:
    - Spells worse scale (both layers and number of bullets)
    ## Cirno:
    ### Spells:
    - Patterns now have 50% more bullets
    ### Boss:
    - Every Number of Bullets Scale twice as hard with Boss Lvl expect number of pellets
    - Blue Shard Attack is much faster (speed from 1-3.5 to 2-4.5)
    ## Mystia:
    ### Boss:
    - Green Balls Bullets Pattern now shoot 2x more lines (smaller gaps in resault)
    - Attack where she send one ex Mystia from her position also shoot that Green Balls Bullets Pattern
    ## Tewi:
    ### Shoot:
    - Charge shoot have 2 times bigger range
    ### Spells:
    - Faster and better scaling
    - Spawn much higher up and horizontal position depend on enemy position
    - 2 Layers of Pattern instead of 1
    ### Boss:
    - Ex attack Pattern also shoot small version of 2lvl pattern without pellets from her position
    - Attack where she attack both sides with fans of bullets (also with pellets), she also send some Ex Attacks
    ## Merlin:
    - Faster Charge Speed

---
+++ ReworkPoFV/patch.js
@@ -0,0 +1,11 @@
+{
+    "id": "ReworkPoFV",
+    "servers": [
+        "https://mirrors.thpatch.net/Wensomt/ReworkPoFV/"
+    ],
+    "title": "Basicly Rework Touhou PoFV with Some Balance Changes",
+    "dependencies": [
+        "nmlgc/base_tsa",
+        "zero318/ZeroEclExtensions"
+    ]
+}
--- repo.js
+++ repo.js
@@ -2,7 +2,7 @@
     "contact": "@Wensomt",
     "id": "Wensomt",
     "patches": {
-        "Th9NoNerd": "Remove From Touhou PoFV Nerd mechanics like punishment for using focus, boss countering, and few buffs/nerfs to characters, can hold Z to shoot and X to Charge",
+        "ReworkPoFV": "Basicly Rework Touhou PoFV with Some Balance Changes",
     },
     "servers": [
         "https://mirrors.thpatch.net/Wensomt/"
