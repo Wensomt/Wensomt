a4cf0778f02618f765bb978eb3e0ee49756d2120
Dawid_Wendt <d.wendt@op.pl>
2025-05-16 12:17:01 +0200

    Added new branch

---
+++ PoFV_Balance+Bugfixes/patch.js
@@ -0,0 +1,11 @@
+{
+    "id": "PoFV_Balance+Bugfixes",
+    "servers": [
+        "https://mirrors.thpatch.net/Wensomt/PoFV_Balance+Bugfixes/"
+    ],
+    "title": "Balance Touhou PoFV with bugfixes",
+    "dependencies": [
+        "nmlgc/base_tsa",
+        "zero318/ZeroEclExtensions"
+    ]
+}
--- repo.js
+++ repo.js
@@ -3,6 +3,7 @@
     "id": "Wensomt",
     "patches": {
         "ReworkPoFV": "Basicly Rework Touhou PoFV with Some Balance Changes",
+		"PoFV_Balance+Bugfixes": "Balance Touhou PoFV with bugfixes",
     },
     "servers": [
         "https://mirrors.thpatch.net/Wensomt/"
